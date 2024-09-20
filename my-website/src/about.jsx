b35ca080466c98a3beb19fb7c51a6716f5fed3f9
priya-singh-14 <singh.priya3@northeastern.edu>
2024-09-19 22:01:28 -0400

    created navbar component, formatted text and image on landing page, created about section

---
+++ my-website/src/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+    return ( 
+    <header className="about">
+    <div className='im'>
+        <h1>Hello</h1>
+   </div>
+  </header>
+);
+};
+
+export default About; 
--- my-website/src/header.jsx
+++ my-website/src/header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './header.css';
+import bo from './bo.jpg';
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>Hi, I’m Priya. </h1>
+      <div className='text-section'>
+      <div className='large'>
+      <p>Hi, I’m Priya. </p>
+      </div>
       <p>Developer & Designer. Computer Science and Design Major at Northeastern University.</p>
+     </div>
+     <div className='img-section'>
+     <img src={bo} alt="img"></img>
+     </div>
     </header>
   );
 };
