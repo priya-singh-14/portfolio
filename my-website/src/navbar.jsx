2bac48ae3c048f5c7c857173a4045a8f044bd222
priya-singh-14 <singh.priya3@northeastern.edu>
2024-09-20 19:56:19 -0400

    changed blur color and navbar color to be less stark

--- my-website/src/navbar.jsx
+++ my-website/src/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="ellipse-background">
         <ul className="nav-links">
-          <li><a href="#about">About</a></li>
+          <li><a href="about.jsx">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#gallery">Gallery</a></li>
           <li><a href="#contact">Contact</a></li>
