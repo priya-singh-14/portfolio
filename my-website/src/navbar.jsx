9565289add03f510051a94a45bad844bef67e8af
priya-singh-14 <singh.priya3@northeastern.edu>
2024-09-24 18:44:58 -0400

    wrote functionality for Contact link on navbar to copy my email and fade in with new text, and respective css changes

--- my-website/src/navbar.jsx
+++ my-website/src/navbar.jsx
@@ -1,7 +1,30 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 import './navbar.css'; 
 
 function Navbar() {
+  const [contactText, setContactText] = useState("Contact");
+  const [fadeClass, setFadeClass] = useState(""); 
+  const handleCopyEmail = () => {
+    const email = "singh.priya3@northeastern.edu"; 
+    navigator.clipboard.writeText(email);
+
+    setFadeClass("fade-out");
+
+    setTimeout(() => {
+      setContactText("Email Copied!");
+      setFadeClass("fade-in");
+
+      setTimeout(() => {
+        setFadeClass("fade-out");
+        setTimeout(() => {
+          setContactText("Contact");
+          setFadeClass("fade-in");
+        }, 200); // fade out
+      }, 2000); 
+    }, 400); // fade in
+  };
+
   return (
     <div className="navbar">
       <div className="ellipse-background">
@@ -9,8 +32,10 @@ function Navbar() {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/underconstruction">Contact</Link></li>
-          {/* put function to copy email to clipboard above */}
+          <li> <a className={`contact-button ${fadeClass}`} onClick={handleCopyEmail}>
+            {contactText}
+          </a>
+          </li>
         </ul>
       </div>
     </div>
