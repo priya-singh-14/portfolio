51d2d73f544e03298dfee19e2a0aefc65c1f2f0c
priya-singh-14 <singh.priya3@northeastern.edu>
2024-10-02 13:27:11 -0400

    Update App.js

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Navbar from './navbar.jsx';
 import About from './about.jsx';
